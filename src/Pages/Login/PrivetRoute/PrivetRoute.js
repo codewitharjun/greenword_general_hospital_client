57971915c4a45000eaa05139e626860018685083
Arjun Roy <arjunroy.aroy@gmail.com>
2021-10-19 01:03:09 +0600

    Fixed Privet Router

--- src/Pages/Login/PrivetRoute/PrivetRoute.js
+++ src/Pages/Login/PrivetRoute/PrivetRoute.js
@@ -3,25 +3,24 @@ import {Spinner} from 'react-bootstrap';
 import { Redirect, Route } from "react-router";
 import useAuth from "../../../hooks/useAuth";
 
- const PriverRoute = ({children, ...rest}) => {
+ const PriverRoute = ({ children, ...rest }) => {
 
     const {user, isLoading} = useAuth();
     if (isLoading){
-    return <Spinner animation="border" varient="danger" />
+    return <Spinner animation="grow" varient="danger" />
     }
 
     return (
-    <Route
-    {...rest}
-    render={({location}) => user.email ? children : <Redirect
-    to = {{
-        pathname: "/login",
-        state: {from: location}
-    }}
+        <Route
+        {...rest}
+        render={({ location }) => user.email ? children : <Redirect
+        to={{
+            pathname: "/login",
+            state: { from: location }
+          }}
     ></Redirect>
 }
     >
-        <h2>Please Login</h2>
     </Route>
     );
 };
